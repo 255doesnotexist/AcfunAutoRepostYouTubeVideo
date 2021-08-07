ca35ca06bcf2f4186fb60a7bf7f2b8d4ea7ac677
ezrameow <1293883574jcy@gmail.com>
2021-08-07 17:55:54 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+API_KEY = "AIzaSyBdbQ-WPIkQkEad2EtRPfbRMiMURPxyqm8"  # Google Data (YouTube v3 key) 不保证此key可用性，有需要请自己在Google Developers里申请
+CHANNELS_WATCHING_ON = ['UCnGsBkjtNacIi9qX3WbU20Q'] # 被实时监视的YouTube频道
+STOP_SCAN_ID = [''] # 填上扫描截止视频ID（从前往后搬运到这个视频就停止搬运）
